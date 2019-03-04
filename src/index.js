b21aab1789fb37bd69071eaecbf0d89d1d4f33c5
Daniel Thompson-Yvetot <35242872+nothingismagick@users.noreply.github.com>
2019-03-04 15:51:30 +0100

    feat(refactor) initial scaffolding for app-extension
    Notes:
     - removed imagemin-advpng from dependencies
     - remove tunnel-agent patch for advpng from dependencies
     - added test app and prerigged with yarn link

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/**
+ * Quasar App Extension index/runner script
+ * (runs on each dev/build)
+ *
+ * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/IndexAPI.js
+ */
+
+module.exports = function (api, ctx) {
+  //
+}
---
+++ src/prompts.js
@@ -0,0 +1,40 @@
+/**
+ * Quasar App Extension prompts script
+ *
+ * Inquirer prompts
+ * (answers are available as "api.prompts" in the other scripts)
+ * https://www.npmjs.com/package/inquirer#question
+ *
+ * Example:
+
+  return [
+    name: {
+      type: 'string',
+      required: true,
+      message: 'Quasar CLI Extension name (without prefix)',
+    },
+    preset: {
+      type: 'checkbox',
+      message: 'Check the features needed for your project:',
+      choices: [
+        {
+          name: 'Install script',
+          value: 'install'
+        },
+        {
+          name: 'Prompts script',
+          value: 'prompts'
+        },
+        {
+          name: 'Uninstall script',
+          value: 'uninstall'
+        }
+      ]
+    }
+  ]
+
+ */
+
+module.exports = function () {
+  return []
+}
