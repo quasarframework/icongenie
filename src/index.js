fcd604e95dc4404a55c600053334842f250b4324
TobyMosque <tobias.mesquita@gmail.com>
2019-03-10 14:54:40 -0300

    feat(refactor) Get rid of the intermediary folder and make the settings editable. (#34)
    * configurable options
    
    * intermeiate folders

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ const fs = require('fs-extra'),
   deasync = require('deasync'),
   iconfactory = require('../lib/index.js')
 
-const { validatePng, computeHash, createConfig } = require('./utils')
+const { validatePng, computeHash, getConfig, saveConfig } = require('./utils')
+const useIntermediateFolders = false
 
 console.log=(function() {
   // log, log, its big its heavy its wood.
@@ -28,92 +29,14 @@ const copy = (src, dest) => {
 const copyFiles = async (target, modeName, retries = 0) => {
   switch (modeName) {
     case 'spa':
-      await copy(target + '/spa/icon-16x16.png', './src/statics/icons/favicon-16x16.png')
-      await copy(target + '/spa/icon-32x32.png', './src/statics/icons/favicon-32x32.png')
-
-      // since pwa mode create a spa folder, so there is a chance of the spa folder didn't have all need icons.
-      let root = fs.existsSync(target + '/spa/icon-128x128.png') ? '/spa/' : '/pwa/generic-'
-      await copy(target + root + 'icon-128x128.png', './src/statics/quasar-logo.png')
-      await copy(target + root + 'icon-128x128.png', './src/statics/icons/icon-128x128.png')
-      await copy(target + root + 'icon-192x192.png', './src/statics/icons/icon-192x192.png')
-      await copy(target + root + 'icon-256x256.png', './src/statics/icons/icon-256x256.png')
-      await copy(target + root + 'icon-384x384.png', './src/statics/icons/icon-384x384.png')
-      await copy(target + root + 'icon-512x512.png', './src/statics/icons/icon-512x512.png')
-      break
     case 'pwa':
-      await copy(target + '/spa/icon-16x16.png', './src/statics/icons/favicon-16x16.png')
-      await copy(target + '/spa/icon-32x32.png', './src/statics/icons/favicon-32x32.png')
-
-      await copy(target + '/pwa/generic-icon-128x128.png', './src/statics/quasar-logo.png')
-      await copy(target + '/pwa/generic-icon-128x128.png', './src/statics/icons/icon-128x128.png')
-      await copy(target + '/pwa/generic-icon-192x192.png', './src/statics/icons/icon-192x192.png')
-      await copy(target + '/pwa/generic-icon-256x256.png', './src/statics/icons/icon-256x256.png')
-      await copy(target + '/pwa/generic-icon-384x384.png', './src/statics/icons/icon-384x384.png')
-      await copy(target + '/pwa/generic-icon-512x512.png', './src/statics/icons/icon-512x512.png')
-
-      await copy(target + '/pwa/ms-icon-144x144.png', './src/statics/icons/ms-icon-144x144.png')
-      await copy(target + '/pwa/apple-icon-152x152.png', './src/statics/icons/apple-icon-152x152.png')
+      await copy(target, './src/statics')
       break
     case 'electron':
-      await copy(target + '/electron', './src-electron/icons')
-      await copy(target + '/electron/icon.icns', './src-electron/icons/icon.icns')
-      await copy(target + '/electron/icon.ico', './src-electron/icons/icon.ico')
-      await copy(target + '/electron/icon.png', './src-electron/icons/icon.png')
-      await copy(target + '/electron/linux-512x512', './src-electron/icons/linux-512x512')
-      break
+      await copy(target, './src-electron/icons')
+      break;
     case 'cordova':
-      await copy(target + '/cordova/android/ldpi.png', './src-cordova/res/icon/android/icon-36-ldpi.png')
-      await copy(target + '/cordova/android/mdpi.png', './src-cordova/res/icon/android/icon-48-mdpi.png')
-      await copy(target + '/cordova/android/hdpi.png', './src-cordova/res/icon/android/icon-72-hdpi.png')
-      await copy(target + '/cordova/android/xhdpi.png', './src-cordova/res/icon/android/icon-96-xhdpi.png')
-      await copy(target + '/cordova/android/xxhdpi.png', './src-cordova/res/icon/android/icon-144-xxhdpi.png')
-      await copy(target + '/cordova/android/xxxhdpi.png', './src-cordova/res/icon/android/icon-192-xxxhdpi.png')
-
-      await copy(target + '/cordova/ios/icon@2x.png', './src-cordova/res/icon/ios/icon-57-2x.png')
-      await copy(target + '/cordova/ios/icon.png', './src-cordova/res/icon/ios/icon-57.png')
-      await copy(target + '/cordova/ios/icon-40@2x.png', './src-cordova/res/icon/ios/icon-40-2x.png')
-      await copy(target + '/cordova/ios/icon-40.png', './src-cordova/res/icon/ios/icon-40.png')
-      await copy(target + '/cordova/ios/icon-50@2x.png', './src-cordova/res/icon/ios/icon-50-2x.png')
-      await copy(target + '/cordova/ios/icon-50.png', './src-cordova/res/icon/ios/icon-50.png')
-      await copy(target + '/cordova/ios/icon-60@3x.png', './src-cordova/res/icon/ios/icon-60-3x.png')
-      await copy(target + '/cordova/ios/icon-60@2x.png', './src-cordova/res/icon/ios/icon-60-2x.png')
-      await copy(target + '/cordova/ios/icon-60.png', './src-cordova/res/icon/ios/icon-60.png')
-      await copy(target + '/cordova/ios/icon-72@2x.png', './src-cordova/res/icon/ios/icon-72-2x.png')
-      await copy(target + '/cordova/ios/icon-72.png', './src-cordova/res/icon/ios/icon-72.png')
-      await copy(target + '/cordova/ios/icon-76@2x.png', './src-cordova/res/icon/ios/icon-76-2x.png')
-      await copy(target + '/cordova/ios/icon-76.png', './src-cordova/res/icon/ios/icon-76.png')
-      await copy(target + '/cordova/ios/icon-83.5@2x.png', './src-cordova/res/icon/ios/icon-83.5-2x.png')
-      await copy(target + '/cordova/ios/icon-167.png', './src-cordova/res/icon/ios/icon-167.png')
-      await copy(target + '/cordova/ios/icon-1024.png', './src-cordova/res/icon/ios/icon-1024.png')
-
-      await copy(target + '/cordova/windows/Square48x48Logo.png', './src-cordova/res/icon/windows-phone/icon-48.png')
-      await copy(target + '/cordova/windows/Square62x62Logo.png', './src-cordova/res/icon/windows-phone/icon-62-tile.png')
-      await copy(target + '/cordova/windows/Square173x173Logo.png', './src-cordova/res/icon/windows-phone/icon-173-tile.png')
-
-      await copy(target + '/cordova/android/hdpi-screen-landscape.png', './src-cordova/res/screen/android/screen-hdpi-landscape.png')
-      await copy(target + '/cordova/android/hdpi-screen-portrait.png', './src-cordova/res/screen/android/screen-hdpi-portrait.png')
-      await copy(target + '/cordova/android/ldpi-screen-landscape.png', './src-cordova/res/screen/android/screen-ldpi-landscape.png')
-      await copy(target + '/cordova/android/ldpi-screen-portrait.png', './src-cordova/res/screen/android/screen-ldpi-portrait.png')
-      await copy(target + '/cordova/android/mdpi-screen-landscape.png', './src-cordova/res/screen/android/screen-mdpi-landscape.png')
-      await copy(target + '/cordova/android/mdpi-screen-portrait.png', './src-cordova/res/screen/android/screen-mdpi-portrait.png')
-      await copy(target + '/cordova/android/xhdpi-screen-landscape.png', './src-cordova/res/screen/android/screen-xhdpi-landscape.png')
-      await copy(target + '/cordova/android/xhdpi-screen-portrait.png', './src-cordova/res/screen/android/screen-xhdpi-portrait.png')
-      await copy(target + '/cordova/android/xxhdpi-screen-landscape.png', './src-cordova/res/screen/android/screen-xxhdpi-landscape.png')
-      await copy(target + '/cordova/android/xxhdpi-screen-portrait.png', './src-cordova/res/screen/android/screen-xxhdpi-portrait.png')
-      await copy(target + '/cordova/android/xxxhdpi-screen-landscape.png', './src-cordova/res/screen/android/screen-xxxhdpi-landscape.png')
-      await copy(target + '/cordova/android/xxxhdpi-screen-portrait.png', './src-cordova/res/screen/android/screen-xxxhdpi-portrait.png')
-
-      await copy(target + '/cordova/ios/screen-ipad-landscape-2x.png', './src-cordova/res/screen/ios/screen-ipad-landscape-2x.png')
-      await copy(target + '/cordova/ios/screen-ipad-landscape.png', './src-cordova/res/screen/ios/screen-ipad-landscape.png')
-      await copy(target + '/cordova/ios/screen-ipad-portrait-2x.png', './src-cordova/res/screen/ios/screen-ipad-portrait-2x.png')
-      await copy(target + '/cordova/ios/screen-ipad-portrait.png', './src-cordova/res/screen/ios/screen-ipad-portrait.png')
-      await copy(target + '/cordova/ios/screen-iphone-landscape-2x.png', './src-cordova/res/screen/ios/screen-iphone-landscape-2x.png')
-      await copy(target + '/cordova/ios/screen-iphone-landscape.png', './src-cordova/res/screen/ios/screen-iphone-landscape.png')
-      await copy(target + '/cordova/ios/screen-iphone-portrait-2x.png', './src-cordova/res/screen/ios/screen-iphone-portrait-2x.png')
-      await copy(target + '/cordova/ios/screen-iphone-portrait-568h-2x.png', './src-cordova/res/screen/ios/screen-iphone-portrait-568h-2x.png')
-      await copy(target + '/cordova/ios/screen-iphone-portrait.png', './src-cordova/res/screen/ios/screen-iphone-portrait.png')
-
-      await copy(target + '/cordova/windows/Splashscreen-480x800.png', './src-cordova/res/screen/windows-phone/screen-portrait.png')
+      await copy(target, './src-cordova/res')
       break
   }
 }
@@ -135,40 +58,51 @@ const initialize = async function(api, ctx, config) {
     modeName = config.ssr.pwa ? 'pwa' : 'spa'
   }
 
-  let target = './icon-factory/' + mode
-  let processImagess = async function() {
-    await iconfactory[modeName](source, target, minify)
-    iconConfig.source[mode] = hash
-    iconConfig.target[modeName][mode] = hash
-    fs.writeFile(configFileName, JSON.stringify(iconConfig, null, 2), err => {
-      if (err) throw err
-    })
-  }
-
-  let configFileName = './quasar.icon-factory.json'
-  await validatePng(source)
-  if (fs.existsSync(configFileName)) {
-    iconConfig = await new Promise(resolve => {
-      fs.readFile(configFileName, 'utf8', (err, data) => {
-        let _config = JSON.parse(data)
-        resolve(_config)
-      })
-    })
+  let target = ''
+  if (useIntermediateFolders) {
+    target = './icon-factory/' + mode + '/' + modeName
   } else {
-    iconConfig = await createConfig(api.prompts)
+    switch (modeName) {
+      case 'spa':
+      case 'pwa':
+        target = './src/statics'
+        break
+      case 'electron':
+        target = './src-electron/icons'
+        break;
+      case 'cordova':
+        target = './src-cordova/res'
+        break
+    }
+  }
+  
+  let processImagess = async function() {
+    await iconfactory[modeName](source, target, minify, iconConfig.options[modeName], true)
+    iconConfig.modes[mode].source = hash
+    if (useIntermediateFolders) {
+      iconConfig.modes[mode].targets[modeName] = hash
+    } else {
+      iconConfig.targets[modeName] = hash
+    }
+    saveConfig(iconConfig)
   }
 
+  await validatePng(source)
+  iconConfig = await getConfig(api.prompts)
   hash = await computeHash(source, 'md5', minify)
   fs.ensureDirSync(target)
-  if (!fs.existsSync(target + '/' + modeName)) {
+  let targetHash = useIntermediateFolders ? iconConfig.modes[mode].targets[modeName] : iconConfig.targets[modeName]
+  if (!fs.existsSync(target)) {
     await processImagess()
-  } else if (iconConfig.source[mode] !== hash) {
+  } else if (iconConfig.modes[mode].source !== hash) {
     await processImagess()
-  } else if (iconConfig.target[modeName][mode] !== hash) {
+  } else if (targetHash !== hash) {
     await processImagess()
   }
 
-  await copyFiles(target, modeName, 0)
+  if (useIntermediateFolders) {
+    await copyFiles(target, modeName, 0)
+  }
   // TODO: move icons to the correct place
 }
 
